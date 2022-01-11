16efa086e8895d742d21c5e336cae8678391ecbd
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2022-01-11 13:26:30 -0700

    continue database setup and page structure

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Photo Sharing</title>
+</head>
+
+<body>
+    <h1>Photo Sharing</h1>
+    <hr>
+    <a href="uploadPhoto.php"><button>Upload Photo</button></a>
+    <hr>
+    <h2>Photos</h2>
+</body>
+
+</html>
